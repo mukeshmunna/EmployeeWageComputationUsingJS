a019a05988226c4fe29e1fc62a62b0d97b3c5c9b
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-09-07 14:49:12 +0530

    [Mukesh] Add : implement the calculation of wage for the employee

--- EmpWage.js
+++ EmpWage.js
@@ -15,3 +15,15 @@ if (empCheck == IS_ABSENT) {
   console.log("Employee is present");
 }
 
+//UC2
+let emphrs = 5;
+empCheck = Math.floor(Math.random() * 10) % 3;
+switch (empCheck) {
+    case IS_PART_TIME: emphrs = PART_TIME_HR;
+        break;
+    case IS_FULL_TIME: emphrs = FULL_TIME_HR
+        break;
+    case IS_ABSENT: emphrs = IS_ABSENT;
+}
+let empWage = emphrs * WAGE_PER_HR;
+console.log(empWage);
