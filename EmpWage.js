fe339ace7b01ec360af53a89f97e3d7bed293c81
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-09-11 12:37:38 +0530

    [Mukesh] Add : Array Wage Function With Array Helper function

--- EmpWage.js
+++ EmpWage.js
@@ -60,11 +60,11 @@ console.log(totalEmpwage);
 let  hrs=0;
 for(let i=0;i<NUM_OF_WORKING_DAYS;i++)
 {
-    empcheck=Math.floor(Math.random()*10)%3;
-    hrs+=getworkinghrs(empcheck);
+    empCheck=Math.floor(Math.random()*10)%3;
+    hrs+=getWorkingHours(empCheck);
 }
-totalempwage=hrs*WAGE_PER_HR;
-console.log(totalempwage);
+totalEmpwage=hrs*WAGE_PER_HR;
+console.log(totalEmpwage);
 
 //UC5
 let totalemphr=0;
@@ -73,7 +73,7 @@ while(totalemphr<MAX_HRS_IN_A_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
 {
     totalWorkingDays++;
     let empcheck=Math.floor(Math.random()*10)%3;
-    totalemphr+=getworkinghrs(empcheck);
+    totalemphr+=getWorkingHours(empcheck);
 } 
 let empwage1 = totalemphr*WAGE_PER_HR;
 console.log("Total Days :"+totalWorkingDays+"Total hours: "+totalemphr+"Emp Wage :"+empwage1);
@@ -91,9 +91,55 @@ const DailyWageArray = new Array();
 while (totalEmpHours < MAX_HRS_IN_A_MONTH && totalWorkingDay < NUM_OF_WORKING_DAYS) {
     totalWorkingDay++;
     empCheck = Math.floor(Math.random() * 10) % 3;
-    TotalEmphrs += getworkinghrs(empCheck);
+    TotalEmphrs += getWorkingHours(empCheck);
     DailyWageArray.push(CalculateDailyWage(TotalEmphrs));
 }
 let empWage1 = CalculateDailyWage(TotalEmphrs);
 console.log("Total days :" + totalWorkingDay + " Total Hours : " + TotalEmphrs + " Emp Wages " + empWage1);
 console.log(DailyWageArray)
+
+//UC7
+//A
+totalEmpwage = 0;
+function sum(dailyWage){
+    totalEmpwage += dailyWage;
+}
+DailyWageArray.forEach(sum);
+console.log(totalEmpwage);
+
+//B
+let dayCount = 0;
+function mapDayWithWage(dailyWage){
+    dayCount++;
+    return dayCount+"  "+dailyWage;
+}
+let mapWithDayArray = DailyWageArray.map(mapDayWithWage);
+console.log(DailyWageArray);
+
+//C
+function fullTimeWage(dailyWage){
+    return dailyWage.includes("160");
+}
+let fullTimeWageArr = mapWithDayArray.filter(fullTimeWage);
+console.log(fullTimeWageArr);
+
+//D
+console.log(mapWithDayArray.find(fullTimeWage));
+
+//E
+console.log(fullTimeWageArr.every(fullTimeWage));
+
+//F
+function partTimeWage(dailyWage) {
+    return dailyWage.includes("80");
+}
+console.log(mapWithDayArray.some(partTimeWage));
+
+//G
+function totolDayWorked(numOfDays , dailyWage){
+    if(dailyWage > 0){
+        numOfDays++;
+    }
+    return numOfDays;
+}
+console.log(DailyWageArray.reduce(totolDayWorked,0));
